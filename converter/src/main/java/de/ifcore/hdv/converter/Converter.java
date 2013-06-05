73559a8b68b6896862989905c03fbb3a46af35dc
Adrian Stabiszewski <as@grundid.de>
2013-06-05 10:38:30 +0200

    initiales projekt

---
+++ converter/src/main/java/de/ifcore/hdv/converter/Converter.java
@@ -0,0 +1,7 @@
+package de.ifcore.hdv.converter;
+
+public class Converter {
+
+	public static void main(String[] args) {
+	}
+}
---
+++ converter/src/test/java/de/ifcore/hdv/converter/ConverterTest.java
@@ -0,0 +1,11 @@
+package de.ifcore.hdv.converter;
+
+import org.junit.Test;
+
+public class ConverterTest {
+
+	@Test
+	public void itShouldRunConverter() throws Exception {
+		Converter.main(null);
+	}
+}
