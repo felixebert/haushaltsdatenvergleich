c8b9077dc5d81ec85b5f1599ceb3a3b43cef5daa
Adrian Stabiszewski <as@grundid.de>
2013-06-12 11:00:23 +0200

    ausgabe dateiname kann jetzt definiert werden

--- converter/src/main/java/de/ifcore/hdv/converter/Converter.java
+++ converter/src/main/java/de/ifcore/hdv/converter/Converter.java
@@ -5,21 +5,22 @@ import de.ifcore.hdv.converter.data.MergedData;
 public class Converter {
 
 	public static void main(String[] args) {
-		if (args.length == 4) {
+		if (args.length == 5) {
 			String incomeFile = args[0];
 			String spendingsFile = args[1];
 			String population = args[2];
 			String areaSize = args[3];
+			String outputFile = args[4];
 			try {
 				BaseConverter baseConverter = new BaseConverter(incomeFile, spendingsFile, population, areaSize);
 				MergedData mergedData = baseConverter.createMergedData();
-				baseConverter.writeData(mergedData, "hdv.json");
+				baseConverter.writeData(mergedData, outputFile);
 			}
 			catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 		else
-			System.out.println("Usage: <income> <spendings> <population> <areaSize>");
+			System.out.println("Usage: <income> <spendings> <population> <areaSize> <output-file>");
 	}
 }
