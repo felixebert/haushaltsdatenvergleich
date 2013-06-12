2250001c0a1ef77f04c36958e9d5dfe39f01cc76
Adrian Stabiszewski <as@grundid.de>
2013-06-12 13:25:29 +0200

    reihenfolge der werte geändert

--- converter/src/main/java/de/ifcore/hdv/converter/CountyConverter.java
+++ converter/src/main/java/de/ifcore/hdv/converter/CountyConverter.java
@@ -2,24 +2,26 @@ package de.ifcore.hdv.converter;
 
 import de.ifcore.hdv.converter.data.MergedData;
 
-public class CountryConverter {
+public class CountyConverter {
 
 	public static void main(String[] args) {
-		if (args.length == 4) {
+		if (args.length == 5) {
 			String incomeFile = args[0];
 			String spendingsFile = args[1];
 			String population = args[2];
 			String areaSize = args[3];
+			String outputFile = args[4];
 			try {
 				BaseConverter baseConverter = new BaseConverter(incomeFile, spendingsFile, population, areaSize);
+				//TODO group by county
 				MergedData mergedData = baseConverter.createMergedData();
-				baseConverter.writeData(mergedData, "hdv.json");
+				baseConverter.writeData(mergedData, outputFile);
 			}
 			catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 		else
-			System.out.println("Usage: <income> <spendings> <population> <areaSize>");
+			System.out.println("Usage: <income> <spendings> <population> <areaSize> <output>");
 	}
 }
