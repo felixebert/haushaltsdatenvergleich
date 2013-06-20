b16fbdb5174eec566d8bb6945dc9ff838188e760
Felix Ebert <felix.ebert@if-core.de>
2013-06-20 18:42:20 +0200

    javascript formatting

--- viewer/web/js/app.js
+++ viewer/web/js/app.js
@@ -1,16 +1,16 @@
 'use strict';
 var hdv = {
-	defaults : {
-		lat : 51.463,
-		lon : 7.18,
-		zoom : 9,
-		pg : 523,
-		compare : 'in',
-		relation : 'none',
-		areaLayer : 'gemeinden',
-		year : 2009
+	defaults: {
+		lat: 51.463,
+		lon: 7.18,
+		zoom: 9,
+		pg: 523,
+		compare: 'in',
+		relation: 'none',
+		areaLayer: 'gemeinden',
+		year: 2009
 	},
-	initDefaults : function() {
+	initDefaults: function() {
 		var search = window.location.search;
 		if (search != "") {
 			var params = search.substr(1).split('&');
