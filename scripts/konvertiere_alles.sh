da5ec198361cd0a56c623159752603def9abac69
Felix Ebert <felix.ebert@if-core.de>
2013-07-08 11:27:55 +0200

    scripts/ verzeichnis, .sh scripts

---
+++ scripts/konvertiere_alles.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+./konvertiere_bilanz.sh
+./konvertiere_finanzdaten_gemeinden.sh
+./konvertiere_finanzdaten_landkreise.sh
---
+++ scripts/konvertiere_finanzdaten_gemeinden.cmd
@@ -0,0 +1,7 @@
+@echo off
+echo "Konvertiere Jahr 2009..."
+java -Xmx3G -Dpretty.json=false -cp ..\converter\target\converter-1.0-SNAPSHOT-jar-with-dependencies.jar de.ifcore.hdv.converter.Converter "..\converter\data\finanzrechnungsstatistik-einnahmen-2009.csv" "..\converter\data\finanzrechnungsstatistik-ausgaben-2009.csv" "..\converter\data\Zensus Einwohnerzahlen.xls" "..\converter\data\Gemeinde Einwohnerzahlen Flaechen 31-03-2013.xls" "..\viewer\web\data\2009\gemeinden"
+echo "Konvertiere Jahr 2010..."
+java -Xmx3G -Dpretty.json=false -cp ..\converter\target\converter-1.0-SNAPSHOT-jar-with-dependencies.jar de.ifcore.hdv.converter.Converter "..\converter\data\finanzrechnungsstatistik-einnahmen-2010.csv" "..\converter\data\finanzrechnungsstatistik-ausgaben-2010.csv" "..\converter\data\Zensus Einwohnerzahlen.xls" "..\converter\data\Gemeinde Einwohnerzahlen Flaechen 31-03-2013.xls" "..\viewer\web\data\2010\gemeinden"
+echo "Konvertiere Jahr 2011..."
+java -Xmx3G -Dpretty.json=false -cp ..\converter\target\converter-1.0-SNAPSHOT-jar-with-dependencies.jar de.ifcore.hdv.converter.Converter "..\converter\data\finanzrechnungsstatistik-einnahmen-2011.csv" "..\converter\data\finanzrechnungsstatistik-ausgaben-2011.csv" "..\converter\data\Zensus Einwohnerzahlen.xls" "..\converter\data\Gemeinde Einwohnerzahlen Flaechen 31-03-2013.xls" "..\viewer\web\data\2011\gemeinden"
